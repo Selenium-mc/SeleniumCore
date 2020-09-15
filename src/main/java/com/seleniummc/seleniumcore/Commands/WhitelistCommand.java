aa1b371f535381e2d1a9351b3ded56062d833162
Alexander Park <Parkalex380@gmail.com>
2020-09-14 20:39:53 -0500

    Added basic imports for whitelist command

--- src/main/java/com/seleniummc/seleniumcore/Commands/WhitelistCommand.java
+++ src/main/java/com/seleniummc/seleniumcore/Commands/WhitelistCommand.java
@@ -1,5 +1,12 @@
 package com.seleniummc.seleniumcore.Commands;
 
+import com.hackclub.hccore.HCCorePlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 public class WhitelistCommand {
 
 }
